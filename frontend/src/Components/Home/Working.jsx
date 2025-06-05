03f58823a94df66e200cb01b8ca1e569380f3811
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-06-05 19:42:03 +0530

    development
    Create the folder structure

--- frontend/src/Components/Home/Working.jsx
+++ frontend/src/Components/Home/Working.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {motion} from 'framer-motion'
 import {slideUpVariants, zoomInVariants} from './animation'
-import {planning} from '../export'
+import {planning} from '../../export'
 
 const Working = () => {
   return (

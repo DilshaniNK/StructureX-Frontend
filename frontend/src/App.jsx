a97f206670e1017e285f2339177b728e4aad293c
ShashiniMadhu <shashinimadushika413@gmail.com>
2025-06-09 23:23:12 +0530

    Common navbar and sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
+//Example page import
+import Example from './Pages/Example'
+
 //financial officer pages
 import FO_Dashboard from './Pages/Financial_officer/Dashboard'
 import FO_Projects from './Pages/Financial_officer/Projects'
 import FO_Payments from './Pages/Financial_officer/Payments'
-
 import Home from './Pages/Home'
 import Admin from './Pages/Admin'
+import Designer from './Pages/Designer/Designer'
 
 const App = () => {
   return (
@@ -17,9 +20,11 @@ const App = () => {
           <Route>
             <Route path="/" element={<Home/>}/>
             <Route path="/admin/*" element={<Admin/>}/>
+            <Route path="/example/*" element={<Example/>}/>
             <Route path="/financial_officer/dashboard" element={<FO_Dashboard/>}/>
             <Route path="/financial_officer/projects" element={<FO_Projects/>}/>
             <Route path="/financial_officer/payments" element={<FO_Payments/>}/>
+            <Route path="/designer/*" element={<Designer/>}/>
           </Route>
         </Routes>
       </Router>
---
+++ frontend/src/Pages/Designer/Designer.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+//https://planner5d.com/use/free-floor-plan-creator
+import Navbar from '../../Components/Employee/Navbar'
+import Sidebar from '../../Components/Employee/Sidebar' 
+
+export default function Home() {
+  const userRole = 'Designer'; // Change this based on your user's actual role
+  const userName = 'John Doe'; // Get this from your user context/state
+
+  const navigate = useNavigate();
+
+  const handleSidebarNavigate = (id, path) => {
+    navigate(path); // This will navigate when a Sidebar item is clicked
+  };
+
+  return (
+    <div>
+      <Navbar 
+        userRole={userRole}
+        userName={userName}
+      />
+      <Sidebar 
+        userRole={userRole} 
+        activeItem="overview"
+        onNavigate={handleSidebarNavigate}
+      />
+      
+      <div className="pt-20 p-8 ml-18">
+        Hello
+      </div>
+    </div>
+  );
+}

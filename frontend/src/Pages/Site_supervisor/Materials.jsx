ad6f5b5c476a76148ba9de1af36be153f1c05123
ShaniGamage <shaninehansa716@gmail.com>
2025-06-11 21:15:59 +0530

    SS_Inventory and labors pages

--- frontend/src/Pages/Site_supervisor/Materials.jsx
+++ frontend/src/Pages/Site_supervisor/Materials.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+// MaterialsPage.js
+import Inventory from '../../Components/Site_supervisor/InventoryRequest';
 
-const Materials = () => {
+const materials = [
+  { name: 'Cement Bags' },
+  { name: 'Sand (tons)'},
+  { name: 'Steel Rods'},
+];
+
+const projects = ['Site A', 'Site B', 'Site C'];
+
+export default function MaterialsPage() {
   return (
-    <div>
-      Materials management for Site Supervisor
-    </div>
-  )
+    <Inventory
+      title="Materials Request Form"
+  type="Materials"
+  items={materials}
+  projects={[
+    'Colombo Highrise Project',
+    'Galle Road Mall Construction',
+  ]}
+    />
+  );
 }
-
-export default Materials

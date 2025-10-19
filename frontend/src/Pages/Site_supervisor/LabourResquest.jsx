fa9578eacc75643f4bfb2dc936c2c53eb0cf1683
shani gamage <shaninehansa716@gmail.com>
2025-10-19 18:53:43 +0530

    ss - labour request

---
+++ frontend/src/Pages/Site_supervisor/LabourResquest.jsx
@@ -0,0 +1,16 @@
+// ToolsPage.js
+import Inventory from '../../Components/Site_supervisor/InventoryRequest';
+
+
+
+const projects = ['Site A', 'Site B', 'Site C'];
+
+export default function ToolsPage() {
+  return (
+    <Inventory
+      title="Labour Request Form"
+  type="Labor"
+  
+    />
+  );
+}
